6d21ffa2a67119d48ea34b564f25ae396c827343
pqv199x <pqv199x@hotmail.com>
2022-03-02 10:47:14 +0700

    calculate decimal

--- src/strategies/dao-lp/index.ts
+++ src/strategies/dao-lp/index.ts
@@ -161,7 +161,7 @@ export async function strategy(
         stakedTDAO.map((value, i) => [
             addresses[i],
             //   parseFloat(formatUnits(value.toString(), options.decimals))
-            parseFloat(new BigNumber(value).div(options.decimals).toString(10))
+            parseFloat(new BigNumber(value).div(10 ** parseInt(options.decimals)).toString(10))
         ])
     );
 }
